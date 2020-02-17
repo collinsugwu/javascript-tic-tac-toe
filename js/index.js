c213f3ed1816d78d041d43a55c1d34ced594c39c
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-02-17 14:55:33 +0100

    ui page added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // let buttonCount = 9;
 });
+
+document.getElementById('startgame').addEventListener('click', () => {
+  document.querySelector('.table').style.display = 'table';
+})  
