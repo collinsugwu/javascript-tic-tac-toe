1b881acc018b2f0b13f04d731e8abd7c4fe7a385
collins <ugwucollins64@gmail.com>
2020-03-15 20:08:04 +0100

    test completed

--- test/board.test.js
+++ test/board.test.js
@@ -1,7 +1,25 @@
 const board = require('../src/board');
 
-test('check position of symbol', () => {
-  expect(board.Game().position(2)).toBe('X');
+const validateResult = (symbol, arrayPosition) => {
+  // horizontal
+  if (arrayPosition[0] + arrayPosition[1] + arrayPosition[2] === symbol.repeat(3)
+    || arrayPosition[3] + arrayPosition[4] + arrayPosition[5] === symbol.repeat(3)
+    || arrayPosition[6] + arrayPosition[7] + arrayPosition[8] === symbol.repeat(3)) return true;
+
+  // vertical
+  if (arrayPosition[0] + arrayPosition[3] + arrayPosition[6] === symbol.repeat(3)
+    || arrayPosition[1] + arrayPosition[4] + arrayPosition[7] === symbol.repeat(3)
+    || arrayPosition[2] + arrayPosition[5] + arrayPosition[8] === symbol.repeat(3)) return true;
+
+  // diagonal
+  if (arrayPosition[0] + arrayPosition[4] + arrayPosition[8] === symbol.repeat(3)
+    || arrayPosition[2] + arrayPosition[4] + arrayPosition[6] === symbol.repeat(3)) return true;
+
+  return false;
+};
+
+test('check position of symbol to be empty string', () => {
+  expect(board.Game().position(2)).toBe(undefined);
 });
 
 test('validate position taken', () => {
@@ -16,6 +34,23 @@ test('check if board is not full', () => {
   });
 });
 
-// test('validate position taken', () => {
-//   expect(board.Game().validatePosition('')).toBe('Position Taken');
-// });
+test('player X as the winner', () => {
+  const boardArr = ['X', 'X', 'X', 'O', ' ', ' ', ' ', ' ', ' '];
+  const symbol = 'X';
+  const result = validateResult(symbol, boardArr);
+  expect(result).toEqual(true);
+});
+
+test('player O as the winner', () => {
+  const boardArr = ['O', 'O', 'O', 'X', ' ', ' ', ' ', ' ', ' '];
+  const symbol = 'O';
+  const result = validateResult(symbol, boardArr);
+  expect(result).toEqual(true);
+});
+
+test("it's a draw", () => {
+  const boardArr = ['X', 'O', 'X', 'O', 'X', 'O', 'O', 'X', 'O'];
+  const symbol = 'O';
+  const result = validateResult(symbol, boardArr);
+  expect(result).toEqual(false);
+});
